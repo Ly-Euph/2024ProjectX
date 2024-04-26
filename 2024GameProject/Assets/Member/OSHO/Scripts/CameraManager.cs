9a8887d530c9b5d4bb39beaeb77589bbd2266076
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2024-04-26 10:54:44 +0900

    一旦コミ。特に変更はなし

--- 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
+++ 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
@@ -10,11 +10,28 @@ public class CameraManager : MonoBehaviour
     [SerializeField] GameObject Camera3;
     [SerializeField] GameObject Camera4;
 
-    public Rect newviewport;
+    public Text CameraNumber1;
+    public Text CameraNumber2;
+    public Text CameraNumber3;
+    public Text CameraNumber4;
+
+    //カメラの切り替え
 
     void Start()
     {
-       
+        Camera1 = GameObject.Find("Camera1");
+        Camera2 = GameObject.Find("Camera2");
+        Camera3 = GameObject.Find("Camera3");
+        Camera4 = GameObject.Find("Camera4");
+        
+        //カメラは最初にfalseに
+        Camera2.SetActive(false);
+        Camera3.SetActive(false);
+        Camera4.SetActive(false);
+
+        CameraNumber2.enabled = false;
+        CameraNumber3.enabled = false;
+        CameraNumber4.enabled = false;
     }
 
     void Update()
@@ -22,22 +39,55 @@ public class CameraManager : MonoBehaviour
         //番号１
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-           
+            Camera1.SetActive(true);
+            Camera2.SetActive(false);
+            Camera3.SetActive(false);
+            Camera4.SetActive(false);
+
+            //Textをenabledで管理
+            CameraNumber1.enabled = true;
+            CameraNumber2.enabled = false;
+            CameraNumber3.enabled = false;
+            CameraNumber4.enabled = false;
         }
         //番号２
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-           
+            Camera2.SetActive(true);
+            Camera1.SetActive(false);
+            Camera3.SetActive(false);
+            Camera4.SetActive(false);
+
+            CameraNumber2.enabled = true;
+            CameraNumber1.enabled = false;
+            CameraNumber3.enabled = false;
+            CameraNumber4.enabled = false;
         }
         //番号３
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-           
+            Camera3.SetActive(true);
+            Camera1.SetActive(false);
+            Camera2.SetActive(false);
+            Camera4.SetActive(false);
+
+            CameraNumber3.enabled = true;
+            CameraNumber1.enabled = false;
+            CameraNumber2.enabled = false;
+            CameraNumber4.enabled = false;
         }
         //番号４
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-           
+            Camera4.SetActive(true);
+            Camera1.SetActive(false);
+            Camera2.SetActive(false);
+            Camera3.SetActive(false);
+
+            CameraNumber4.enabled = true;
+            CameraNumber1.enabled = false;
+            CameraNumber2.enabled = false;
+            CameraNumber3.enabled = false;
         }
     }
 }
