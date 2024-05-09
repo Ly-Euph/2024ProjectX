e711fea3ed4bfe543527a83aa7b716a4bb74405e
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2024-05-09 14:55:15 +0900

    スキャン機能仮

--- 2024GameProject/Assets/Member/OSHO/Scripts/CameraMove.cs
+++ 2024GameProject/Assets/Member/OSHO/Scripts/CameraMove.cs
@@ -9,17 +9,20 @@ public class CameraMove : MonoBehaviour
     public int CamMin = 30;
     public int CamMax = 50;
 
-    [SerializeField] float MaxrotPos = -50;
+    //カメラの横振りの制御をシリアライズ化
+    [SerializeField] float MaxrotPos = 50;
     [SerializeField] float MinrotPos = -50;
+
     // Start is called before the first frame update
     void Start()
     {
         cam = gameObject.GetComponent<Camera>();
     }
-
+    int f =0;
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.localRotation.y);
         if(Input.GetKey(KeyCode.W) && cam.fieldOfView > CamMin)
         {
             cam.fieldOfView -= 0.5f;
@@ -29,13 +32,14 @@ public class CameraMove : MonoBehaviour
         {
             cam.fieldOfView += 0.5f;
         }
-        if(Input.GetKey(KeyCode.A) && transform.position.y <= MinrotPos)
+        if(Input.GetKey(KeyCode.A) && transform.localRotation.y * 180 / 3.14>= MinrotPos)
         {
-            transform.Rotate(new Vector3(0, -50, 0));
+            transform.Rotate(new Vector3(0, -1, 0));
+  
         }
-        if(Input.GetKey(KeyCode.D) && transform.position.y <= MaxrotPos)
+        if(Input.GetKey(KeyCode.D) && transform.localRotation.y * 180 / 3.14 <= MaxrotPos)
         {
-            transform.Rotate(new Vector3(0, 50, 0));
+            transform.Rotate(new Vector3(0, 1, 0));
         }
     }
 }
