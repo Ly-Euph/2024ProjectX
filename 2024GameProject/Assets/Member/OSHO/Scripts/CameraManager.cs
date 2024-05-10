e72d577dcb3413797cc37eb11c9404f75f8ab8ad
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2024-05-10 11:14:19 +0900

    gg

--- 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
+++ 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
@@ -5,54 +5,61 @@ using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] GameObject Camera1;
-    [SerializeField] GameObject Camera2;
-    [SerializeField] GameObject Camera3;
-    [SerializeField] GameObject Camera4;
+    [SerializeField] GameObject[] Camera = new GameObject[3];
+    [SerializeField] Text[] CameraNumber = new Text[3];
 
     SonarFx sf1;
     SonarFx sf2;
     SonarFx sf3;
     SonarFx sf4;
 
-    public Text CameraNumber1;
-    public Text CameraNumber2;
-    public Text CameraNumber3;
-    public Text CameraNumber4;
-
-    GameObject[] sfobj;
+    int i = 0;
 
     void Start()
     {
-        Camera1 = GameObject.Find("Camera1");
-        Camera2 = GameObject.Find("Camera2");
-        Camera3 = GameObject.Find("Camera3");
-        Camera4 = GameObject.Find("Camera4");
+        Camera[0] = GameObject.Find("Camera1");
+        Camera[1] = GameObject.Find("Camera2");
+        Camera[2] = GameObject.Find("Camera3");
+        Camera[3] = GameObject.Find("Camera4");
         
         //カメラ2,3,4は最初にfalseに
-        Camera2.SetActive(false);
-        Camera3.SetActive(false);
-        Camera4.SetActive(false);
+        Camera[1].SetActive(false);
+        Camera[2].SetActive(false);
+        Camera[3].SetActive(false);
 
         //テキスト2,3,4は最初falseに
-        CameraNumber2.enabled = false;
-        CameraNumber3.enabled = false;
-        CameraNumber4.enabled = false;
+        CameraNumber[1].enabled = false;
+        CameraNumber[2].enabled = false;
+        CameraNumber[3].enabled = false;
 
-        sf1 = Camera1.GetComponent<SonarFx>();
-        sf2 = Camera2.GetComponent<SonarFx>();
-        sf3 = Camera3.GetComponent<SonarFx>();
-        sf4 = Camera4.GetComponent<SonarFx>();
+        sf1 = Camera[0].GetComponent<SonarFx>();
+        sf2 = Camera[1].GetComponent<SonarFx>();
+        sf3 = Camera[2].GetComponent<SonarFx>();
+        sf4 = Camera[3].GetComponent<SonarFx>();
 
         //Sonartx2,3,4をfalseに
+        sf1.enabled = false;
         sf2.enabled = false;
         sf3.enabled = false;
         sf4.enabled = false;
-
     }
 
     void Update()
     { 
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            sf1.enabled = true;
+            sf2.enabled = true;
+            sf3.enabled = true;
+            sf4.enabled = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            sf1.enabled = false;
+            sf2.enabled = false;
+            sf3.enabled = false;
+            sf4.enabled = false;
+        }
         //番号１
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
@@ -81,77 +88,86 @@ public class CameraManager : MonoBehaviour
 
     void SetCamera1()
     {
-        Camera1.SetActive(true);
-        Camera2.SetActive(false);
-        Camera3.SetActive(false);
-        Camera4.SetActive(false);
+        Camera[0].SetActive(true);
+        Camera[1].SetActive(false);
+        Camera[2].SetActive(false);
+        Camera[3].SetActive(false);
 
         //Textをenabledで管理
-        CameraNumber1.enabled = true;
-        CameraNumber2.enabled = false;
-        CameraNumber3.enabled = false;
-        CameraNumber4.enabled = false;
+        CameraNumber[0].enabled = true;
+        CameraNumber[1].enabled = false;
+        CameraNumber[2].enabled = false;
+        CameraNumber[3].enabled = false;
 
-       
-        sf1.enabled = true;
-        sf2.enabled = false;
-        sf3.enabled = false;
-        sf4.enabled = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sf1.enabled = true;
+            sf2.enabled = false;
+            sf3.enabled = false;
+            sf4.enabled = false;
+        }
     }
 
     void SetCamera2()
     {
-        Camera2.SetActive(true);
-        Camera1.SetActive(false);
-        Camera3.SetActive(false);
-        Camera4.SetActive(false);
+        Camera[1].SetActive(true);
+        Camera[0].SetActive(false);
+        Camera[2].SetActive(false);
+        Camera[3].SetActive(false);
 
-        CameraNumber2.enabled = true;
-        CameraNumber1.enabled = false;
-        CameraNumber3.enabled = false;
-        CameraNumber4.enabled = false;
+        CameraNumber[1].enabled = true;
+        CameraNumber[0].enabled = false;
+        CameraNumber[2].enabled = false;
+        CameraNumber[3].enabled = false;
 
-       
-        sf2.enabled = true;
-        sf3.enabled = false;
-        sf4.enabled = false;
-        sf1.enabled = false;
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sf2.enabled = true;
+            sf3.enabled = false;
+            sf4.enabled = false;
+            sf1.enabled = false;
+        }
     }
 
     void SetCamera3()
     {
-        Camera3.SetActive(true);
-        Camera1.SetActive(false);
-        Camera2.SetActive(false);
-        Camera4.SetActive(false);
+        Camera[2].SetActive(true);
+        Camera[0].SetActive(false);
+        Camera[1].SetActive(false);
+        Camera[3].SetActive(false);
 
-        CameraNumber3.enabled = true;
-        CameraNumber1.enabled = false;
-        CameraNumber2.enabled = false;
-        CameraNumber4.enabled = false;
+        CameraNumber[2].enabled = true;
+        CameraNumber[0].enabled = false;
+        CameraNumber[1].enabled = false;
+        CameraNumber[3].enabled = false;
 
-        sf3.enabled = true;
-        sf4.enabled = false;
-        sf1.enabled = false;
-        sf2.enabled = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sf3.enabled = true;
+            sf4.enabled = false;
+            sf1.enabled = false;
+            sf2.enabled = false;
+        }
     }
 
     void SetCamera4()
     {
-        Camera4.SetActive(true);
-        Camera1.SetActive(false);
-        Camera2.SetActive(false);
-        Camera3.SetActive(false);
+        Camera[3].SetActive(true);
+        Camera[0].SetActive(false);
+        Camera[1].SetActive(false);
+        Camera[2].SetActive(false);
 
-        CameraNumber4.enabled = true;
-        CameraNumber1.enabled = false;
-        CameraNumber2.enabled = false;
-        CameraNumber3.enabled = false;
+        CameraNumber[3].enabled = true;
+        CameraNumber[0].enabled = false;
+        CameraNumber[1].enabled = false;
+        CameraNumber[2].enabled = false;
 
-        sf4.enabled = true;
-        sf1.enabled = false;
-        sf2.enabled = false;
-        sf3.enabled = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sf4.enabled = true;
+            sf1.enabled = false;
+            sf2.enabled = false;
+            sf3.enabled = false;
+        }
     }
 }
