1dd743d7e1fdfd2f1cafb84777b132eb56bad209
DESKTOP-67MT2K8\user <nkc20226199@st.denpa.jp>
2024-05-21 13:29:21 +0900

    no message

--- 2024GameProject/Assets/Member/Evening/Script/Duct.cs
+++ 2024GameProject/Assets/Member/Evening/Script/Duct.cs
@@ -12,6 +12,20 @@ public class Duct : MonoBehaviour
     Vector3 ductPos;
 
     private int random;
+    private int[] enemyCost = { 1, 2, 2, 3 };       //敵ごとのコスト
+    private int _enemyCost;                         //現在のコスト
+    private int maxEnemyCost;                       //最大コスト
+
+    private int[] typeCost = { 0, 0, 0, 0 };        //種別のコスト
+    private int[] maxTypeCost = { 3, 2, 2, 1 };     //種別の最大コスト
+
+    private int[] randEnemy1 = { 1, 1, 2, 1, 2, 2, 1, 3, 1, 1, 2, 1, 2, 2, 3, 2, 1, 2, 1, 2 };      //☆1
+    private int[] randEnemy2 = { 3, 1, 4, 1, 1, 1, 4, 3, 4, 2, 2, 3, 1, 1, 1, 1, 1, 1, 3, 2 };      //☆1
+    private int[] randEnemy3 = { 2, 2, 1, 1, 2, 1, 3, 4, 3, 4, 1, 3, 2, 1, 3, 2, 2, 3, 1, 1 };      //☆2
+    private int[] randEnemy4 = { 2, 2, 3, 2, 1, 2, 3, 1, 1, 4, 3, 2, 2, 3, 1, 1, 1, 3, 3, 1 };      //☆2
+    private int[] randEnemy5 = { 1, 4, 3, 2, 2, 2, 3, 1, 4, 4, 4, 3, 1, 1, 4, 2, 3, 4, 3, 3 };      //☆3
+    private int[] randEnemy6 = { 2, 4, 4, 1, 3, 1, 1, 4, 3, 1, 4, 2, 3, 2, 3, 3, 1, 3, 3, 1 };      //☆3
+
     private float timer;
 
     // Start is called before the first frame update
@@ -21,42 +35,34 @@ public class Duct : MonoBehaviour
         ductPos = duct.transform.position;
 
         timer = 0;
+
+        _enemyCost = 0;
+        maxEnemyCost = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-
-        if(timer>=1f)
+        if(timer>=2f)
         {
-            random = Random.Range(1, 5);
+            //どの敵かランダム選択
+            random = Random.Range(0, Enemy.Length);
 
-            switch(random)
+            if (_enemyCost < maxEnemyCost && typeCost[random] < maxTypeCost[random])
             {
-                case 1:
-                    Instantiate(Enemy[0], ductPos, Quaternion.identity);
-                    break;
+                //各個体の最大コストより小さい場合実行
+                Instantiate(Enemy[random], ductPos, Quaternion.identity);
+                _enemyCost += enemyCost[random];
+                Debug.Log("現在の敵全体コスト" + _enemyCost);
 
-                case 2:
-                    Instantiate(Enemy[1], ductPos, Quaternion.identity);
-                    break;
-
-                case 3:
-                    Instantiate(Enemy[2], ductPos, Quaternion.identity);
-                    break;
-
-                case 4:
-                    Instantiate(Enemy[3], ductPos, Quaternion.identity);
-                    break;
+                typeCost[random]++;
+                Debug.Log("現在の種別コスト { " + typeCost[0] + " , " + typeCost[1] + " , " + typeCost[2] + " , " + typeCost[3] + " }");
 
             }
-                
-
-
-            //Instantiate(Enemy1, ductPos, Quaternion.identity);
             timer = 0;
         }
 
+        
     }
 }
