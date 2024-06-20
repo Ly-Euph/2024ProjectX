435cb7518295cd3eba67267c3c99267b45a2f942
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2024-06-20 11:33:51 +0900

    gg

---
+++ 2024GameProject/Assets/Member/OSHO/Scripts/Collision_system.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_system : MonoBehaviour
+{
+    //インターフェース（敵とのダメージ処理）用のint変数
+
+    public int Em_damage = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("敵に触れました。");
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+        damageable.TakeDamage(Em_damage);
+    }
+}
