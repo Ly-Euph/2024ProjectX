61084a1bd784b9236340b0f24618e09b7fa944f2
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2024-06-28 14:36:31 +0900

    Scriptの改良。

--- 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
+++ 2024GameProject/Assets/Member/OSHO/Scripts/CameraManager.cs
@@ -31,10 +31,6 @@ public class CameraManager : MonoBehaviour
 
     [SerializeField] GameObject[] Trap_GK;
 
-    [Header("それぞれのScanUIを入れるとこ")]
-
-    [SerializeField] Image Scan_UI;
-
     [Header("それぞれのCamZoomUIを入れるとこ")]
     
     [SerializeField] GameObject[] Cam_Zoom;
