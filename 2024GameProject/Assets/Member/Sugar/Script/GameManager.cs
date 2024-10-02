51bf541fe1d00d564d9a094612418c608d550337
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2024-10-02 10:39:59 +0900

    ステージBGM

--- 2024GameProject/Assets/Member/Sugar/Script/GameManager.cs
+++ 2024GameProject/Assets/Member/Sugar/Script/GameManager.cs
@@ -43,23 +43,14 @@ public class GameManager : MonoBehaviour
 
     // 仮で必要になりそうなものを準備。
     // 追加で必要ならここに用意　
-    // キャラクター関連の音
-    //public enum ClipSe
-    //{
-    //    Move1,
-    //    Move2,
-    //    Jump1,
-    //    Jump2,
-    //    Atk1,
-    //    Atk2,
-    //    Hit1,
-    //    Hit2,
-    //    Skill1,
-    //    Skill2,
-    //    Death,
-    //    num,
-    //}
+
+    // オブジェクトとかいろいろ
     public enum ClipSe
+    {
+       stage,
+    }
+    // キャラクター関連の音
+    public enum HUMANClipSe
     {
         Move1,
         Move2,
