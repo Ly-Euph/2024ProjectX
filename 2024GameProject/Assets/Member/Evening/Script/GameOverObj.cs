f8ab4a84f1c207e57615ebf89f1dd4e1cc08df19
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2024-10-06 23:16:19 +0900

    調整

--- 2024GameProject/Assets/Member/Evening/Script/GameOverObj.cs
+++ 2024GameProject/Assets/Member/Evening/Script/GameOverObj.cs
@@ -1,26 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class GameOverObj : MonoBehaviour
 {
-    bool START = false;    // シート切り替えタイミング管理 
-
-    // シーン遷移に使う
-    [SerializeField] Fade fade;
-
     bool detection = false;         //敵検知
 
-    // Update is called once per frame
-    void Update()
+    public bool SendDetection
     {
-        if(detection==true&&!START)
-        {
-            START = true;
-            Debug.Log("death");
-            fade.FadeIn(0.5f, () => SceneManager.LoadScene("GameOverScene"));
-        }
+        get { return detection; }
     }
 
     private void OnTriggerEnter(Collider col)
