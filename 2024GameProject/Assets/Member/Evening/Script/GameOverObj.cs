81065e671704a64c1392fe87ccdaf7cccb15c398
DESKTOP-67MT2K8\user <pipiyangult@gmail.com>
2025-02-18 20:37:00 +0900

    カメラのクールタイム実装敵の修正

--- 2024GameProject/Assets/Member/Evening/Script/GameOverObj.cs
+++ 2024GameProject/Assets/Member/Evening/Script/GameOverObj.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class GameOverObj : MonoBehaviour
 {
     bool detection = false;         //“GŒŸ’m
 
+    bool IsEnd = false;
+
+    [SerializeField] Fade fade;
+
+
     public bool SendDetection
     {
         get { return detection; }
     }
 
+    void GameOver()
+    {
+        if (!IsEnd)
+        {
+            IsEnd = true;
+            fade.FadeIn(0.5f, () => SceneManager.LoadScene("GameOverScene"));
+        }
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         detection = true;
+        GameOver();
     }
 
 }
